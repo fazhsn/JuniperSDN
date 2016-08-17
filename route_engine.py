f92a54dd0f938c44e8a718b3cd6447202abae36a
heliamarasinghe <heliamarasinghe@yahoo.com>
2016-08-16 22:25:21 -0400

    whatever

--- route_engine.py
+++ route_engine.py
@@ -3,10 +3,14 @@ requests.packages.urllib3.disable_warnings()
 import json
 from shortestpaths import EppsteinShortestPathAlgorithm
 import networkx as nx
+from LatencyListen import CurrentLinkUtil
+from LinkAPI import getNodesDict
+from LinkObj import Link
 
+def get_nodes():
+	nodesDict = {"1":
 
-
-def createTopologyGraph():
+def createTopologyGraph(nodes, links):
 	graph = nx.DiGraph() # Directed Graph
 	graph.add_node('s', name = "source", index= 's')
 	graph.add_node('t', name = "destination",index='t')
@@ -20,13 +24,16 @@ def createTopologyGraph():
 	graph.add_weighted_edges_from(edges)
 	return graph
 
-
-
-
+def get_alt_path(lspList, brknLink):
+	nodes = getNodesDict()
+	links = CurrentLinkUtil()
+	graph = createTopologyGraph(nodes, links)
 
 
 def main():
-    
+	brknLink = "L_10.210.24.2_10.210.24.1"
+	lspList = ["GROUP_ONE_SF_NY_LSP1", "GROUP_ONE_NY_SF_LSP1"]
+    	get_alt_path(lspList, brknLink)
 
 
 if __name__ == "__main__":
